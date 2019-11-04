7619c74f3e8015512c3f93801711a148d2c97667
Adam Jimenez <jimenez.adam0@gmail.com>
2019-11-04 18:35:23 -0500

    particpants

--- index.js
+++ index.js
@@ -4,10 +4,55 @@ const solver = require("./solver")
 const app = express()
 
 const schema = {
-    "teamName": "teamName",
+    "teamName": "𓂺",
     "teamStreetAddress": "11 rue du Calvados",
     "solutions": null,
-    "participants": []
+    "participants": [
+        {
+            "isCaptain": true,
+            "fullName": "Adam Jimenez",
+            "email": "jimenez.adam0@gmail.com",
+            "googleAccount": "jimenez.adam0@gmail.com",
+            "phone": "(438) 797-7794",
+            "school": "École de Technologie Supérieure",
+            "schoolProgram": "Génie Logiciel",
+            "graduationDate": 1620187200000,
+            "shirtSize": "m-M"
+        },
+        {
+            "isCaptain": false,
+            "fullName": "Guillaume Baril",
+            "email": "gbaril@gmail.com",
+            "googleAccount": "gbaril@gmail.com",
+            "phone": "(438) 505-9237",
+            "school": "École de Technologie Supérieure",
+            "schoolProgram": "Génie Logiciel",
+            "graduationDate": 1620187200000,
+            "shirtSize": "m-XL"
+        },
+        {
+            "isCaptain": false,
+            "fullName": "Jonathan Lalande",
+            "email": "lalande.gg@gmail.com",
+            "googleAccount": "lalande.gg@gmail.com",
+            "phone": "(514) 706-3076",
+            "school": "École de Technologie Supérieure",
+            "schoolProgram": "Génie Logiciel",
+            "graduationDate": 1597529582000,
+            "shirtSize": "m-M"
+        },
+        {
+            "isCaptain": false,
+            "fullName": "David Sylvestre",
+            "email": "david.sylvestre27@gmail.com",
+            "googleAccount": "david.sylvestre27@gmail.com",
+            "phone": "(514) 796-7337",
+            "school": "École de Technologie Supérieure",
+            "schoolProgram": "Génie Logiciel",
+            "graduationDate": 1587268800,
+            "shirtSize": "m-M"
+        }
+    ]
 }
 
 const letters = []
